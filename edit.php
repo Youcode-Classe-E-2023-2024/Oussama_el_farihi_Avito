f845e07835cc3fa2c07986f4629e0275387b062f
oussamaef06 <oussamaelfarihi@gmail.com>
2023-11-17 12:23:00 +0100

    add php code and front code to edit product

--- edit.php
+++ edit.php
@@ -1,32 +1,29 @@
 <?php
-include 'connection.php';
+include('connection.php');
 
-// Fetch product details for editing
-$product_id = $_GET['id'];
-$select_query = "SELECT * FROM annonce WHERE id = ?";
-$stmt = $conn->prepare($select_query);
-$stmt->bind_param('i', $product_id);
-$stmt->execute();
-$result = $stmt->get_result();
-$product = $result->fetch_assoc();
+// Fetch the rows from the 'product' table
+$sql = "SELECT * FROM annonce";
+$result = mysqli_query($conn, $sql);
 
-// Process product update
-if (isset($_POST['update'])) {
+// Handle form submission for updating products
+if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['update'])) {
+    $product_id = $_POST['product_id'];
     $product_title = $_POST['product_title'];
     $product_description = $_POST['product_description'];
     $product_price = $_POST['product_price'];
 
-    $update_query = "UPDATE annonce SET titre = ?, description = ?, prix = ? WHERE id = ?";
-    $stmt = $conn->prepare($update_query);
-    $stmt->bind_param('ssdi', $product_title, $product_description, $product_price, $product_id);
+    // Update the product details
+    $update_sql = "UPDATE annonce SET titre=?, description=?, prix=? WHERE id=?";
+    $stmt = $conn->prepare($update_sql);
+    $stmt->bind_param('sssi', $product_title, $product_description, $product_price, $product_id);
 
     if ($stmt->execute()) {
-        echo "<script>alert('Product updated successfully'); window.location.href='dashboard.php';</script>";
-        exit();
+        echo "<script>alert('Product updated successfully');</script>";
     } else {
         echo "<script>alert('Failed to update product: " . $stmt->error . "');</script>";
     }
 }
+
 ?>
 
 <!DOCTYPE html>
@@ -36,48 +33,51 @@ if (isset($_POST['update'])) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <script src="https://cdn.tailwindcss.com"></script>
-    <title>Edit Product</title>
+    <title>Edit Products</title>
 </head>
 
 <body>
     <div class="min-h-full">
         <nav class="bg-gray-800">
-            <!-- Navigation code here... (same as dashboard.php) -->
+            <!-- Navigation code -->
         </nav>
-
         <header class="bg-white shadow">
-            <!-- Header code here... (same as dashboard.php) -->
-        </header>
-
-        <main>
             <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
-                <div class="mt-6">
-                    <h2 class="text-2xl font-bold leading-7 text-gray-900">Edit Product</h2>
+                <h1 class="text-3xl font-bold tracking-tight text-gray-900">Edit Products</h1>
+            </div>
+        </header>
+        <main>
+            <div class="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
+                <!-- Display products for editing -->
+                <div class="grid grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-3">
+                    <?php
+                    while ($row = mysqli_fetch_assoc($result)) {
+                        $product_id = $row['id'];
+                        $product_title = $row['titre'];
+                        $product_description = $row['description'];
+                        $product_price = $row['prix'];
+                        $product_picture = $row['image'];
+
+                        echo "
+                        <div class='p-6 bg-white border rounded-lg shadow-md'>
+                            <img src='img/$product_picture' class='w-full h-32 object-cover mb-4'>
+                            <form method='POST'>
+                                <input type='hidden' name='product_id' value='$product_id'>
+                                <label for='product_title' class='block text-sm font-medium text-gray-700'>Title</label>
+                                <input type='text' name='product_title' value='$product_title' class='w-full px-3 py-2 mb-4 border rounded-md focus:outline-none focus:ring focus:border-blue-300'>
+
+                                <label for='product_description' class='block text-sm font-medium text-gray-700'>Description</label>
+                                <textarea name='product_description' class='w-full px-3 py-2 mb-4 border rounded-md focus:outline-none focus:ring focus:border-blue-300'>$product_description</textarea>
+
+                                <label for='product_price' class='block text-sm font-medium text-gray-700'>Price</label>
+                                <input type='text' name='product_price' value='$product_price' class='w-full px-3 py-2 mb-4 border rounded-md focus:outline-none focus:ring focus:border-blue-300'>
+
+                                <button type='submit' name='update' class='bg-blue-500 text-white px-4 py-2 rounded-md hover:bg-blue-700 focus:outline-none focus:ring focus:border-blue-300'>Update</button>
+                            </form>
+                        </div>";
+                    }
+                    ?>
                 </div>
-
-                <form method="POST">
-                    <div class="mt-6 grid grid-cols-1 gap-y-6 gap-x-4 sm:grid-cols-6">
-                        <div class="col-span-6 sm:col-span-4">
-                            <label for="product_title" class="block text-sm font-medium leading-6 text-gray-900">Product Title</label>
-                            <input type="text" name="product_title" id="product_title" autocomplete="street-address" value="<?php echo $product['titre']; ?>" class="mt-1 p-2 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md">
-                        </div>
-
-                        <div class="col-span-6">
-                            <label for="product_description" class="block text-sm font-medium leading-6 text-gray-900">Product Description</label>
-                            <textarea id="product_description" name="product_description" rows="3" class="mt-1 p-2 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"><?php echo $product['description']; ?></textarea>
-                        </div>
-
-                        <div class="col-span-6 sm:col-span-2">
-                            <label for="product_price" class="block text-sm font-medium leading-6 text-gray-900">Product Price</label>
-                            <input type="number" name="product_price" id="product_price" autocomplete="address-level2" value="<?php echo $product['prix']; ?>" class="mt-1 p-2 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md">
-                        </div>
-                    </div>
-
-                    <div class="mt-6 flex items-center justify-end gap-x-6">
-                        <button type="submit" name="update" class="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Update</button>
-                        <a href="dashboard.php" class="text-sm font-semibold leading-6 text-gray-900">Cancel</a>
-                    </div>
-                </form>
             </div>
         </main>
     </div>
